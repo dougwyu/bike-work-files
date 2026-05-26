4bff30e1ca2fa032dfc8a773e173c248f274cefe
Douglas Yu <dougwyu@mac.com>
2026-05-26 09:53:44 +0100

    feat: add manifest and WorkFilesProtocol

---
+++ src/work-files.bkext/dom/protocols.ts
@@ -0,0 +1,14 @@
+import { DOMProtocol } from 'bike/core'
+
+export const DEFAULTS_KEY = 'workfiles.paths'
+
+export interface WorkFilesProtocol extends DOMProtocol {
+  toDOM:
+    | { type: 'init'; paths: string[] }
+    | { type: 'update'; paths: string[] }
+  toApp:
+    | { type: 'add'; path: string }
+    | { type: 'remove'; index: number }
+    | { type: 'reorder'; fromIndex: number; toIndex: number }
+    | { type: 'open'; path: string }
+}
